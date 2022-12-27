d4ffd1dd626a6f4129594d458f75f5661c9f19df
Fir3Storm31 <francesco.catalanotti25>
2022-12-27 01:35:38 +0100

    Customisation implemented

--- FriDa/extractcsv.py
+++ FriDa/extractcsv.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import pathlib
 
-path = pathlib.Path(__file__).parent.resolve()
+def extractcsv():
+    path = pathlib.Path(__file__).parent.resolve()
 
-ds = pd.read_csv(str(path)+'/alumno.csv', sep='|')
+    ds = pd.read_csv(str(path)+'/alumno.csv', sep='|')
 
-headings = ds.columns 
-data = ds.values.tolist()
+    headings = ds.columns 
+    data = ds.values.tolist()
+    return [headings, data]
